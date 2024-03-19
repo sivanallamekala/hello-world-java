bd7537dff49b7795a3af95a3373401b307d69b4e
kuchalakanti <66534502+kuchalakanti@users.noreply.github.com>
2024-03-19 12:49:44 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -34,9 +34,9 @@
  */
 public class HelloWorld {
   public static void main(String[] args) {
-     System.out.println("Hello, Krishna1");
+     System.out.println("goodmorning , Krishna1");
      System.out.println("Hello, World");
      System.out.println("Hello, krishna2");
-     System.out.println("Hello, krishna3");
+     System.out.println("GoodMorning, krishna3");
   }
 }
