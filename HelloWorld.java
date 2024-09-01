8688bb0f3aebde18fac47d7f54eb36b9a260a7a1
kuchalakanti <66534502+kuchalakanti@users.noreply.github.com>
2024-09-01 09:36:51 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -36,7 +36,7 @@ public class HelloWorld {
   public static void main(String[] args) {
      System.out.println("Hello, DevOps");
     System.out.println("Krishna, DevOps");
-    System.out.println("Krishna, Vcube");
+    System.out.println("Krishna, Vcubes");
        
 
   }
