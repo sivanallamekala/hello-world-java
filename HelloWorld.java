a8a0bffb526e8ecdd64e6e63883dc1df12a09105
kuchalakanti <kuchalakanti@gmail.com>
2025-05-30 08:43:05 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -34,7 +34,7 @@
  */
 public class HelloWorld {
   public static void main(String[] args) {
-     System.out.println("Hello, KrishnaDevOps");
+     System.out.println("Hello, KrishnaDevOpss");
      System.out.println("Hello, AWSB05DevOps_AWS011");
      System.out.println("Hello, Jenkinsteams"); 
 
